5c8ce1e61ac43d20920892015f9fc575cc36ead9
bunnyslippers69 <harrison.campbell0101@gmail.com>
2020-05-23 14:55:30 +0100

    AddRole.cs

---
+++ SSRPBalanceBot/Commands/Misc/AddRole.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using SSRPBalanceBot;
+using SSRPBalanceBot.Permissions;
+using System.Linq;
+using Discord.WebSocket;
+
+// Keep in mind your module **must** be public and inherit ModuleBase.
+// If it isn't, it will not be discovered by AddModulesAsync!
+[Group("admin")]
+public class AddRole : ModuleBase<SocketCommandContext>
+{
+    [Command("addrole", RunMode = RunMode.Async)]
+    [Summary("Adds user to role")]
+    public async Task SendRoll(uint id)
+    {
+        var role = Context.Guild.Roles.FirstOrDefault(x => x.Id == id);
+        var member = Context.User as SocketGuildUser;
+
+        await member.AddRoleAsync(role);
+        await Utilities.StatusMessage("addrole", Context);
+    }
+}
